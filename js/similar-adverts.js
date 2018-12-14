e47720cb6c1392d3fac5a7ba8c66c32c2a3351c1
Anton Plechov <bald56rus@gmail.com>
2018-12-15 00:31:57 +0400

    исправление замечаний наставника, сопутствующий рефакторинг

--- js/similar-adverts.js
+++ js/similar-adverts.js
@@ -1,12 +1,19 @@
 'use strict';
 
 (function () {
+  var QUANTITY_VISIBLE_ADVERTS = 5;
+  var HousePrice = {
+    LOW_MAX: 10000,
+    MIDDLE_MIN: 10000,
+    MIDDLE_MAX: 50000,
+    HIGH_MIN: 50000
+  };
   var popupTemplate = document.querySelector('#card').content.querySelector('.map__card');
   var pinTemplate = document.querySelector('#pin').content.querySelector('button');
   var pins = document.querySelector('.map__pins');
   var map = window.map.canvas;
-  var QUANTITY_VISIBLE_SIMILAR_ADVERTS = 5;
   var filterContainer = document.querySelector('.map__filters-container');
+  var filterFields = filterContainer.querySelectorAll('select, input');
   var typeFilter = filterContainer.querySelector('#housing-type');
   var priceFilter = filterContainer.querySelector('#housing-price');
   var roomsFilter = filterContainer.querySelector('#housing-rooms');
@@ -21,11 +28,12 @@
     'housing-guests': 'guests'
   };
   var popupActive = null;
-  var pinActive;
+  var pinActive = null;
   var similarAdverts = [];
-  var typeMap = window.utils.typeMap;
-  var keyCodeMap = window.utils.keyCodeMap;
+  var houseTypeMap = window.utils.houseTypeMap;
+  var KeyCode = window.utils.KeyCode;
   var errorHandler = window.utils.errorHandler;
+  var shuffle = window.utils.shuffleArray;
   var createFeatures = function (parent, features) {
     var destination = parent.querySelector('.popup__features');
     if (features.length === 0) {
@@ -65,14 +73,14 @@
     pinActive = null;
     document.removeEventListener('keydown', escPressHandler);
   };
-  var escPressHandler = window.utils.isKeyPressed(keyCodeMap.ESC, closePopup);
+  var escPressHandler = window.utils.isKeyPressed(KeyCode.ESC, closePopup);
   var createPopup = function (advert) {
     var popup = popupTemplate.cloneNode(true);
     popup.querySelector('.popup__avatar').src = advert.author.avatar;
     popup.querySelector('.popup__title').textContent = advert.offer.title;
     popup.querySelector('.popup__text--address').textContent = advert.offer.address;
     popup.querySelector('.popup__text--price').textContent = advert.offer.price + '₽/ночь.';
-    popup.querySelector('.popup__type').textContent = typeMap[advert.offer.type].name;
+    popup.querySelector('.popup__type').textContent = houseTypeMap[advert.offer.type].name;
     popup.querySelector('.popup__text--capacity').textContent = advert.offer.rooms + ' комнаты для ' + advert.offer.guests + ' гостей';
     popup.querySelector('.popup__text--time', 'Заезд после ' + advert.offer.checkin + ', выезд до ' + advert.offer.checkout);
     createFeatures(popup, advert.offer.features);
@@ -105,27 +113,27 @@
     });
     return pin;
   };
-  var trancateVisibleAdverts = function (adverts) {
-    if (QUANTITY_VISIBLE_SIMILAR_ADVERTS >= adverts.length) {
-      return adverts;
-    }
-    return adverts.slice(0, QUANTITY_VISIBLE_SIMILAR_ADVERTS);
+  var truncateAdverts = function (adverts) {
+    return QUANTITY_VISIBLE_ADVERTS >= adverts.length ? adverts : shuffle(adverts).slice(0, QUANTITY_VISIBLE_ADVERTS);
   };
   var similarBy = function (filter) {
     return function (advert) {
-      return advert.offer[filterMap[filter.id]].toString() === filter.value;
+      return filter.value === 'any' ? true : advert.offer[filterMap[filter.id]].toString() === filter.value;
     };
   };
   var similarByPrice = function (advert) {
+    if (priceFilter.value === 'any') {
+      return true;
+    }
     switch (priceFilter.value) {
       case 'low': {
-        return advert.offer.price < 10000;
+        return advert.offer.price < HousePrice.LOW_MAX;
       }
       case 'middle': {
-        return advert.offer.price >= 10000 && advert.offer.price <= 50000;
+        return advert.offer.price >= HousePrice.MIDDLE_MIN && advert.offer.price <= HousePrice.MIDDLE_MAX;
       }
       case 'high': {
-        return advert.offer.price > 50000;
+        return advert.offer.price > HousePrice.HIGH_MIN;
       }
       default: {
         return false;
@@ -137,37 +145,23 @@
     if (selectedFeatures.length > advertFeatures.length) {
       return false;
     }
-    for (var i = 0; i < selectedFeatures.length; i++) {
-      if (advertFeatures.indexOf(selectedFeatures[i]) === -1) {
-        return false;
-      }
-    }
-    return true;
+    return !selectedFeatures.some(function (feature) {
+      return advertFeatures.indexOf(feature) === -1;
+    });
   };
   var showFilteredAdverts = function () {
     clearSimilarAdverts();
-    var originalAdverts = window.data.get();
-    var filteredAdverts = originalAdverts.slice();
-    if (typeFilter.value !== 'any') {
-      filteredAdverts = filteredAdverts.filter(similarBy(typeFilter));
-    }
-    if (priceFilter.value !== 'any') {
-      filteredAdverts = filteredAdverts.filter(similarByPrice);
-    }
-    if (roomsFilter.value !== 'any') {
-      filteredAdverts = filteredAdverts.filter(similarBy(roomsFilter));
-    }
-    if (guestsFilter.value !== 'any') {
-      filteredAdverts = filteredAdverts.filter(similarBy(guestsFilter));
-    }
-    if (selectedFeatures.length > 0) {
-      filteredAdverts = filteredAdverts.filter(similarByFeatures);
-    }
+    var filteredAdverts = window.data.get()
+      .filter(similarBy(typeFilter))
+      .filter(similarByPrice)
+      .filter(similarBy(roomsFilter))
+      .filter(similarBy(guestsFilter))
+      .filter(similarByFeatures);
     showSimilarAdverts(filteredAdverts);
   };
   var showSimilarAdverts = function (adverts) {
     var _pins = document.createDocumentFragment();
-    trancateVisibleAdverts(adverts).forEach(function (advert) {
+    truncateAdverts(adverts).forEach(function (advert) {
       var pin = createPin(advert);
       similarAdverts.push(pin);
       _pins.appendChild(pin);
@@ -181,30 +175,44 @@
     });
     similarAdverts.length = 0;
   };
+  var filterFieldChangeHandler = function () {
+    timerId = setTimeout(function () {
+      clearTimeout(timerId);
+      showFilteredAdverts();
+    }, 500);
+  };
+  var featuresChangeHandler = function (evt) {
+    if (evt.target.checked) {
+      selectedFeatures.push(evt.target.value);
+    } else {
+      var index = selectedFeatures.indexOf(evt.target.value);
+      selectedFeatures.splice(index, 1);
+    }
+  };
+  var activateFilter = function () {
+    filterFields.forEach(function (field) {
+      field.addEventListener('change', filterFieldChangeHandler);
+    });
+    featuresFilter.forEach(function (feature) {
+      feature.addEventListener('change', featuresChangeHandler);
+    });
+  };
+  var deactivateFilter = function () {
+    filterFields.forEach(function (field) {
+      field.removeEventListener('change', filterFieldChangeHandler);
+    });
+    featuresFilter.forEach(function (feature) {
+      feature.removeEventListener('change', featuresChangeHandler);
+    });
+  };
   window.similarAdverts = {
     showSimilarAdverts: showSimilarAdverts,
-    clearSimilarAdverts: clearSimilarAdverts
+    clearSimilarAdverts: clearSimilarAdverts,
+    activateFilter: activateFilter,
+    deactivateFilter: deactivateFilter
   };
   var initialize = function () {
     window.backend.get('https://js.dump.academy/keksobooking/data', window.data.set, errorHandler);
-    filterContainer.querySelectorAll('select, input').forEach(function (field) {
-      field.addEventListener('change', function () {
-        timerId = setTimeout(function () {
-          clearTimeout(timerId);
-          showFilteredAdverts();
-        }, 500);
-      });
-    });
-    featuresFilter.forEach(function (feature) {
-      feature.addEventListener('change', function (evt) {
-        if (evt.target.checked) {
-          selectedFeatures.push(evt.target.value);
-        } else {
-          var index = selectedFeatures.indexOf(evt.target.value);
-          selectedFeatures.splice(index, 1);
-        }
-      });
-    });
   };
   initialize();
 })();
--- js/utils.js
+++ js/utils.js
@@ -1,18 +1,31 @@
 'use strict';
 
 (function () {
-  var main = document.querySelector('main');
+  var popupContainer = document.querySelector('main');
   var errorTemplate = document.querySelector('#error').content.querySelector('.error');
   var activePopup;
-  var keyCodeMap = {
+  var KeyCode = {
     ESC: 27
   };
-  var typeMap = {
+  var houseTypeMap = {
     flat: {name: 'Квартира', minPrice: 1000},
     bungalo: {name: 'Бунгало', minPrice: 0},
     house: {name: 'Дом', minPrice: 5000},
     palace: {name: 'Дворец', minPrice: 10000}
   };
+  var getRandom = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  var shuffleArray = function (source) {
+    for (var i = 0; i < source.length; i++) {
+      var j = getRandom(0, source.length - 1);
+      var swap = source[i];
+      source[i] = source[j];
+      source[j] = swap;
+    }
+    return source;
+  };
+
   var isKeyPressed = function (keyCode, handler) {
     return function (evt) {
       if (evt.keyCode === keyCode) {
@@ -20,16 +33,15 @@
       }
     };
   };
-
   var closePopupHandler = function () {
     activePopup.remove();
     document.removeEventListener('click', closePopupHandler);
     document.removeEventListener('keydown', escPressHandler);
   };
-  var escPressHandler = isKeyPressed(keyCodeMap.ESC, closePopupHandler);
+  var escPressHandler = isKeyPressed(KeyCode.ESC, closePopupHandler);
   var errorHandler = function () {
     activePopup = errorTemplate.cloneNode(true);
-    main.appendChild(activePopup);
+    popupContainer.appendChild(activePopup);
     var closeBtn = activePopup.querySelector('.error__button');
     closeBtn.addEventListener('click', closePopupHandler);
     document.addEventListener('click', closePopupHandler);
@@ -37,9 +49,11 @@
   };
 
   window.utils = {
-    keyCodeMap: keyCodeMap,
-    typeMap: typeMap,
+    KeyCode: KeyCode,
+    houseTypeMap: houseTypeMap,
     isKeyPressed: isKeyPressed,
-    errorHandler: errorHandler
+    errorHandler: errorHandler,
+    shuffleArray: shuffleArray,
+    popupContainer: popupContainer
   };
 })();
