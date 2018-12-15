787ddc98e54623e2b6c219533735845eabb13a5d
Anton Plechov <bald56rus@gmail.com>
2018-12-15 18:34:40 +0400

    ограничение перемещение пина

--- js/drag-n-drop.js
+++ js/drag-n-drop.js
@@ -1,8 +1,15 @@
 'use strict';
 
 (function () {
+  var map = window.map.canvas;
   var pin = window.map.pin;
-  var pinPosition = {};
+  var PinMoveArea = {
+    MIN_X: 0,
+    MAX_X: map.offsetWidth - pin.clientWidth,
+    MIN_Y: 130,
+    MAX_Y: 630
+  };
+  var position = {};
   pin.addEventListener('mousedown', function (evt) {
     var startPosition = {x: evt.clientX, y: evt.clientY};
     var mouseMoveHandler = function (moveEvt) {
@@ -13,16 +20,20 @@
       };
       startPosition = {x: moveEvt.clientX, y: moveEvt.clientY};
       var top = (pin.offsetTop - offset.y);
-      pinPosition.y = top;
+      if (top >= PinMoveArea.MIN_Y && top <= PinMoveArea.MAX_Y) {
+        position.y = top;
+        pin.style.top = top + 'px';
+      }
       var left = (pin.offsetLeft - offset.x);
-      pinPosition.x = left;
-      pin.style.top = top + 'px';
-      pin.style.left = left + 'px';
+      if (left >= PinMoveArea.MIN_X && left <= PinMoveArea.MAX_X) {
+        position.x = left;
+        pin.style.left = left + 'px';
+      }
     };
     var mouseUpHandler = function (upEvt) {
       upEvt.preventDefault();
       var pinMoveEvent = new CustomEvent('pin-move', {
-        'detail': {position: pinPosition},
+        'detail': {position: position},
         'bubbles': true,
         'cancelable': true
       });
@@ -32,8 +43,5 @@
     };
     document.addEventListener('mousemove', mouseMoveHandler);
     document.addEventListener('mouseup', mouseUpHandler);
-  });
-  pin.addEventListener('mouseup', function () {
-    window.map.activate();
   });
 })();
